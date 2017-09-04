5f4c1f6a8f8c8d6bb2e56e2408869f0fe0a3b80d
civrev <civrev@gmail.com>
2017-09-03 21:16:47 -0400

    breaking open replays

--- cwatts/agents/obs_spy.py
+++ cwatts/agents/obs_spy.py
@@ -14,8 +14,44 @@ class ObsSpyAgent(base.BaseAgent):
 	def step(self, obs):
 		#open the file, with w or write priviledges
 		debug_file = open("/home/christian/StarCraft/cwatts/agents/obs_debug.txt", 'w')
-		debug_file.write('New Obs @')
-		debug_file.write(str(obs))
+		#obs is a TimeStep class
+		#obs.step_type
+		debug_file.write('obs.step_type========\n')
+		debug_file.write(str(obs.step_type))
+		#obs.reward
+		debug_file.write('\nobs.reward========\n')
+		debug_file.write(str(obs.reward))
+		#obs.discount
+		debug_file.write('\nobs.discount========\n')
+		debug_file.write(str(obs.discount))
+		#obs.observation is a dictionary
+		debug_file.write('\nobs.observation.keys]========\n')
+		debug_file.write(str(obs.observation.keys()))
+		debug_file.write('\nobs.observation[single_select]========\n')
+		debug_file.write(str(obs.observation['single_select']))
+		debug_file.write('\nobs.observation[minimap]========\n')
+		debug_file.write(str(obs.observation['minimap']))
+		debug_file.write('\nobs.observation[multi_select]========\n')
+		debug_file.write(str(obs.observation['multi_select']))
+		debug_file.write('\nobs.observation[control_groups]========\n')
+		debug_file.write(str(obs.observation['control_groups']))
+		debug_file.write('\nobs.observation[build_queue]========\n')
+		debug_file.write(str(obs.observation['build_queue']))
+		debug_file.write('\nobs.observation[score_cumulative]========\n')
+		debug_file.write(str(obs.observation['score_cumulative']))
+		debug_file.write('\nobs.observation[cargo_slots_available]========\n')
+		debug_file.write(str(obs.observation['cargo_slots_available']))
+		debug_file.write('\nobs.observation[screen]========\n')
+		debug_file.write(str(obs.observation['screen']))
+		debug_file.write('\nobs.observation[game_loop]========\n')
+		debug_file.write(str(obs.observation['game_loop']))
+		debug_file.write('\nobs.observation[cargo]========\n')
+		debug_file.write(str(obs.observation['cargo']))
+		debug_file.write('\nobs.observation[available_actions]========\n')
+		debug_file.write(str(obs.observation['available_actions']))
+		debug_file.write('\nobs.observation[player]========\n')
+		debug_file.write(str(obs.observation['player']))
+
 
 		#this is the RandomAgent code
 		super(ObsSpyAgent, self).step(obs)
--- cwatts/replay_ripper/read_replay.py
+++ cwatts/replay_ripper/read_replay.py
@@ -1,24 +1,109 @@
 #this is exploratory analysis of .SC2Replay files
-#python3 ~/StarCraft/cwatts/replay_ripper/read_replay.py
+#python3 -m cwatts.replay_ripper.read_replay
 
-from os import walk
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
 import random
 
-replays = []
+from pysc2 import run_configs
 
-#this will go into the directories and make a list of files and folders
-#for each deeper directory it goes into
-#I only care about the first one so I break after one run 
-path = '/home/christian/StarCraftII/Replays/'
-for (dirpath, dirnames, filenames) in walk(path):
-	replays.extend(filenames)
-	break
+from pysc2.lib import app
+from pysc2.lib import gfile
+from s2clientprotocol import sc2api_pb2 as sc_pb
 
-#there are a lot of replays here
-print(len(replays))
+#this is the code from replay_info.py, modified for my purposes
 
-#get one at random
-r = path + replays[random.randrange(0,len(replays))]
-print(r)
+#you run this module without arguments
+def _main(argv=()):
+	replays_x = []
+	path = '/home/christian/StarCraftII/Replays/'
+
+	#this will go into the directories and make a list of files and folders
+	#for each deeper directory it goes into
+	#I only care about the first one so I break after one run 
+	for (dirpath, dirnames, filenames) in os.walk(path):
+		replays_x.extend(filenames)
+		break
+
+	path = path + replays_x[random.randrange(0,len(replays_x))]
+
+	try:
+		if gfile.IsDirectory(path):
+			return _replay_index(path)
+		else:
+			return _replay_info(path)
+	except KeyboardInterrupt:
+		pass
 
 
+def _replay_index(replay_dir):
+	"""Output information for a directory of replays."""
+	run_config = run_configs.get()
+	replay_dir = run_config.abs_replay_path(replay_dir)
+	print("Checking: ", replay_dir)
+
+	with run_config.start() as controller:
+		print("-" * 60)
+		print(",".join((
+			"filename",
+			"build",
+			"map_name",
+			"game_duration_loops",
+			"players",
+			"P1-outcome",
+			"P1-race",
+			"P1-apm",
+			"P2-race",
+			"P2-apm",
+		)))
+
+		bad_replays = []
+		for file_path in run_config.replay_paths(replay_dir):
+			file_name = os.path.basename(file_path)
+			info = controller.replay_info(run_config.replay_data(file_path))
+			if info.HasField("error"):
+				print("failed:", file_name, info.error, info.error_details)
+				bad_replays.append(file_name)
+			else:
+				out = [
+					file_name,
+					info.base_build,
+					info.map_name,
+					info.game_duration_loops,
+					len(info.player_info),
+					sc_pb.Result.Name(info.player_info[0].player_result.result),
+					sc_pb.Race.Name(info.player_info[0].player_info.race_actual),
+					info.player_info[0].player_apm,
+				]
+				if len(info.player_info) >= 2:
+					out += [
+					sc_pb.Race.Name(info.player_info[1].player_info.race_actual),
+					info.player_info[1].player_apm,
+					]
+				print(u",".join(unicode(s) for s in out))
+			if bad_replays:
+				print("Replays with errors:")
+				print("\n".join(bad_replays))
+
+
+def _replay_info(replay_path):
+	"""Query a replay for information."""
+	if not replay_path.lower().endswith("sc2replay"):
+		print("Must be a replay.")
+		return
+
+	run_config = run_configs.get()
+	with run_config.start() as controller:
+		info = controller.replay_info(run_config.replay_data(replay_path))
+	print("-" * 60)
+	print(info)
+
+def main():  # Needed so the setup.py scripts work.
+	app.really_start(_main)
+
+
+if __name__ == "__main__":
+	main()
