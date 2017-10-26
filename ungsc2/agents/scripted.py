55646d9822f5db5744f0917b44c42af5756a1ad1
civrev <civrev@gmail.com>
2017-10-26 12:18:58 -0400

    resize output files

--- ungsc2/agents/scripted.py
+++ ungsc2/agents/scripted.py
@@ -12,7 +12,7 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy
-#numpy.set_printoptions(threshold=numpy.nan)
+numpy.set_printoptions(threshold=numpy.nan)
 
 from ungsc2.agents import base
 from pysc2.lib import actions
@@ -53,8 +53,8 @@ class CollectMineralShards(base.BaseAgent):
 	def step(self, obs):
 		super(CollectMineralShards, self).step(obs)
 		player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
-		player_relative = zoom(player_relative, 0.5)
-		obs_string = '@'.join(player_relative.astype(str).flatten().tolist())
+		p_relative_output = zoom(player_relative, 0.5)
+		obs_string = '@'.join(p_relative_output.astype(str).flatten().tolist())
 		if _MOVE_SCREEN in obs.observation["available_actions"]:
 			neutral_y, neutral_x = (player_relative == _PLAYER_NEUTRAL).nonzero()
 			player_y, player_x = (player_relative == _PLAYER_FRIENDLY).nonzero()
